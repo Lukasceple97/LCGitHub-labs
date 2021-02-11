9d227160d186eb3f62b4c1207e89b36244a3766e
lukas ceplevicius <951814@my.leicestercollege.ac.uk>
2021-02-11 11:37:48 +0000

    newFormsMade

---
+++ Week17-ErrorHandlingWebsite/ErrorProneWebsite.Tests/FileManagerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ErrorProneWebsite.Models;
+using System.IO;
+
+namespace ErrorProneWebsite.Tests
+{
+    [TestClass]
+    public class FileManagerTest
+    {
+        private const string TEST_FILE_PATH = @"C:\Users\Sulur\Documents\Desktop\New folder\Week17-ErrorHandlingWebsite\ErrorProneWebsite.Tests\TestContent\TestContent.txt";
+
+        [TestMethod]
+        public void TheFileManagerCanReadAFile()
+        {
+            FileManager fileManager = new FileManager(TEST_FILE_PATH);
+
+            Assert.AreEqual("Here is some test content.", fileManager.GetContent());
+
+
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void TheFileManagerHandlesAMissingFile()
+        {
+            FileManager fileManager =
+            new FileManager(@"D:\MissingFileThereIsNoFileHere.txt");
+
+            Assert.IsTrue(fileManager.GetEvenMoreContent().Contains("The content file doesn't exist in the location specified!!!!!..."));
+
+        }
+
+
+    }
+}
---
+++ Week25-DataMigration/Week25-DataMigrations/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week25_DataMigrations.Models
+{
+    public class Product
+    {
+        public virtual int ProductId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual decimal Price { get; set; }
+        public virtual string Colour { get; set; }
+    }
+}
